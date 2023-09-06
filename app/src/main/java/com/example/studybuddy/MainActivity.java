f0d6b6b6b24f4915e5c121ba2c395fd3175fcad5
Khoa Nguyễn <nldkhoa21@apcs.fitus.edu.vn>
2023-09-06 18:42:49 +0700

    bottom navigation

--- app/src/main/java/com/example/studybuddy/MainActivity.java
+++ app/src/main/java/com/example/studybuddy/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.studybuddy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseUser currentUser;
+    ActionBar actionBar;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +33,40 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
 
+        actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Home");
+        }
+        loadFragment(new HomeFragment());
 
-
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.home) {
+                actionBar.setTitle("Home");
+                loadFragment(new HomeFragment());
+            } else if (id == R.id.calendar) {
+                actionBar.setTitle("Calendar");
+                loadFragment(new CalendarFragment());
+            } else if (id == R.id.add) {
+                actionBar.setTitle("Add");
+                loadFragment(new AddFragment());
+            } else if (id == R.id.chat) {
+                actionBar.setTitle("Chat");
+                loadFragment(new ChatFragment());
+            } else if (id == R.id.profile) {
+                actionBar.setTitle("Profile");
+                loadFragment(new ProfileFragment());
+            }
+            return true;
+        });
     }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
 }
