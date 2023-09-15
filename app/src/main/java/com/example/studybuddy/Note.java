3ee19fce35a914c6e1b107e8b317df454eaf32c7
dinh-quang-hieu <dqhieu21@apcs.fitus.edu.vn>
2023-09-16 00:54:07 +0700

    Update Calendar

---
+++ app/src/main/java/com/example/studybuddy/Note.java
@@ -0,0 +1,34 @@
+package com.example.studybuddy;
+
+import java.sql.Timestamp;
+
+public class Note
+{
+    String title ,content ;
+
+
+    public Note() {
+    }
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
